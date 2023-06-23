f9dc1ab6db2ce89a668362ee3f72b0b77241e340
newman-afk <84593706+newman-afk@users.noreply.github.com>
2023-06-23 13:41:53 +0800

    mod data for personal blog

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,23 +2,23 @@
 
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Rust Blog',
+  author: 'Rust',
+  headerTitle: 'RustBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://my-blog-newman-afk.vercel.app',
+  siteRepo: 'https://github.com/newman-afk/my-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'hebaorongliyaohui@gmail.com',
+  github: 'https://github.com/newman-afk',
+  //twitter: 'https://twitter.com/Twitter',
+  //facebook: 'https://facebook.com',
+  //youtube: 'https://youtube.com',
+  //linkedin: 'https://www.linkedin.com',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
